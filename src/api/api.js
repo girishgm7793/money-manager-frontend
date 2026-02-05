f39766ccc188605930c64dc83d543d2eaddde20c
girishgm7793 <girishgm@gmail.com>
2026-02-05 15:21:21 +0530

    frontend deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://money-manager-backend-1-fbxi.onrender.com",
 });
 
 export default api;
